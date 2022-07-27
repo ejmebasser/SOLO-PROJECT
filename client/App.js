f402ce1b6481e4bd1935ef18cc17e9a3c957aba8
Emil Mebasser <emil@Emils-Mac-mini.local>
2022-07-27 09:16:53 -0700

    916AM

--- client/App.js
+++ client/App.js
@@ -5,27 +5,35 @@ import StockRow from '../component/StockRow.js';
 
 function App() {
   return (
-    <div className='App'>
-      <div className='container'>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Ticker</th>
-              <th>Price</th>
-              <th>Date</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            <StockRow ticker='aapl' />
-            {/* <StockRow ticker='goog' />
+    <body>
+      <form>
+        <div calss='input-row'>
+          <label for='stock'>Stock Name</label>
+          <input type='text' name='Stock' id='stock' />
+        </div>
+      </form>
+      <div className='App'>
+        <div className='container'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>Ticker</th>
+                <th>Price</th>
+                <th>Date</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              <StockRow ticker='aapl' />
+              {/* <StockRow ticker='goog' />
             <StockRow ticker='msft' />
             <StockRow ticker='tsla' /> */}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
-//ADDING STUFF HERE
+
 export default App;
