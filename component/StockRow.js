0f83fb0447e8a53c2fad225269c3ac618420fffd
Emil Mebasser <emil@Emils-Mac-mini.local>
2022-07-27 07:18:25 -0700

    7/28/2022

--- component/StockRow.js
+++ component/StockRow.js
@@ -14,10 +14,14 @@ class StockRow extends Component {
     //const url = `https://cloud.iexapis.com/stable/stock/aapl/intraday-prices?chartLast=1&token=pk_c6f31cdc66cc485cbe1656f04dffc43f`;
     console.log('LOOK HERE');
     fetch(
-      `https://cloud.iexapis.com/stable/stock/aapl/intraday-prices?&token=pk_c6f31cdc66cc485cbe1656f04dffc43f`
+      `https://cloud.iexapis.com/stable/stock/${this.props.ticker}/intraday-prices?&token=pk_c6f31cdc66cc485cbe1656f04dffc43f`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data[10]));
+      .then((data) => {
+        this.setState({
+          data: data[data.length - 1],
+        });
+      });
   }
   //console.log("look here stupid")
   render() {
